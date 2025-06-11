6bcf09006ab3d9c73c277a3588271588b156d2fb
Gabrie50 <gcriste268@gmail.com>
2025-06-11 20:28:28 -0300

    Update index.ts

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,31 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { MainAreaWidget } from '@jupyterlab/apputils';
+import ResourceMonitor from './components/ResourceMonitor';
 
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-resource-monitor',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    const widget = new MainAreaWidget({ content: ResourceMonitor() });
+    widget.id = 'resource-monitor';
+    widget.title.label = 'Resource Monitor';
+    widget.title.closable = true;
+
+    const command = 'resource-monitor:open';
+    app.commands.addCommand(command, {
+      label: 'Open Resource Monitor',
+      execute: () => {
+        if (!widget.isAttached) app.shell.add(widget, 'main');
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    app.contextMenu.addItem({
+      command,
+      selector: 'body',
+      rank: 0
+    });
+  }
+};
+
+export default plugin;

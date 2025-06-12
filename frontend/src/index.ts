e77539d3696d68b19afe82c4f87f976db071c0ce
Gabrie50 <gcriste268@gmail.com>
2025-06-12 09:50:01 -0300

    Update index.ts

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,31 +1,32 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
-import { MainAreaWidget } from '@jupyterlab/apputils';
-import ResourceMonitor from './components/ResourceMonitor';
+import { ILayoutRestorer, MainAreaWidget } from '@jupyterlab/application';
+import { ReactWidget } from '@jupyterlab/apputils';
+import { ResourceMonitor } from './components/ResourceMonitor';
 
-const plugin: JupyterFrontEndPlugin<void> = {
+class ResourceMonitorWidget extends ReactWidget {
+  constructor() {
+    super();
+    this.addClass('jp-ResourceMonitorWidget');
+  }
+
+  render() {
+    return <ResourceMonitor />;
+  }
+}
+
+const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-resource-monitor',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    const widget = new MainAreaWidget({ content: ResourceMonitor() });
-    widget.id = 'resource-monitor';
+  requires: [ILayoutRestorer],
+  activate: (app: JupyterFrontEnd, restorer: ILayoutRestorer) => {
+    const widget = new MainAreaWidget({ content: new ResourceMonitorWidget() });
+    widget.id = 'resource-monitor-widget';
     widget.title.label = 'Resource Monitor';
     widget.title.closable = true;
 
-    const command = 'resource-monitor:open';
-    app.commands.addCommand(command, {
-      label: 'Open Resource Monitor',
-      execute: () => {
-        if (!widget.isAttached) app.shell.add(widget, 'main');
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    app.contextMenu.addItem({
-      command,
-      selector: 'body',
-      rank: 0
-    });
+    app.shell.add(widget, 'left', { rank: 500 });
+    restorer.add(widget, widget.id);
   }
 };
 
-export default plugin;
+export default extension;
